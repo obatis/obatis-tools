0bc8da9eee4c5e60d1c5ced73b2e2e3edbbebe41
huanglongpu <306315964@qq.com>
2019-05-08 18:11:40 +0800

    优化方法、增加只舍不入，只入不舍的方法

--- src/main/java/com/obatis/constant/http/DefaultHttpConstant.java
+++ src/main/java/com/obatis/constant/http/DefaultHttpConstant.java
@@ -19,7 +19,7 @@ public class DefaultHttpConstant {
 	public static final String REQ_METHOD_POST = "post";
 
 	/**
-	 * 以下类型为微服务框架参数传递使用，主要使用于Spring cloud 微服务分布式开发的项目，单体应用则忽略
+	 * Http 网络请求参数，防止用户信息header 和 认证token等
 	 */
 	/**
 	 * 用户信息传递参数，适用于网关服务将用户信息放置于 header中，微服务项目从header中获取用户信息
