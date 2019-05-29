f1acaaba79905fd530f520de2b7b1b54f909d281
huanglongpu <306315964@qq.com>
2019-05-29 09:44:52 +0800

    新增 url编码、解码方法

--- src/main/java/com/obatis/constant/http/HttpConstant.java
+++ src/main/java/com/obatis/constant/http/HttpConstant.java
@@ -4,7 +4,7 @@ package com.obatis.constant.http;
  * 定义HTTP网络请求基本常量
  * @author HuangLongPu
  */
-public class DefaultHttpConstant {
+public class HttpConstant {
 
 	/**
 	 * HTTP 请求类型，分别定义 POST 和 GET
@@ -12,11 +12,11 @@ public class DefaultHttpConstant {
 	/**
 	 * HTTP 请求类型 POST
 	 */
-	public static final String REQ_METHOD_GET = "get";
+	public static final String METHOD_GET = "get";
 	/**
 	 * HTTP 请求类型 GET
 	 */
-	public static final String REQ_METHOD_POST = "post";
+	public static final String METHOD_POST = "post";
 
 	/**
 	 * Http 网络请求参数，防止用户信息header 和 认证token等
@@ -24,9 +24,9 @@ public class DefaultHttpConstant {
 	/**
 	 * 用户信息传递参数，适用于网关服务将用户信息放置于 header中，微服务项目从header中获取用户信息
 	 */
-	public static final String USER_INFO_HEADER = "userInfo";
+	public static final String HEADER_USER_INFO = "userInfo";
 	/**
 	 * 用户认证token信息头，放置于请求体的header中
 	 */
-	public static final String ACCOUNT_TOKEN_HEADER = "accountToken";
+	public static final String HEADER_ACCOUNT_TOKEN = "accountToken";
 }
