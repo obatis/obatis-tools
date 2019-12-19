a169bc6c96afa56bd2f6ac452e4636e72952681b
huanglongpu <306315964@qq.com>
2019-12-19 21:36:43 +0800

    优化编码转换

--- src/main/java/com/obatis/tools/EncodingTool.java
+++ src/main/java/com/obatis/tools/EncodingTool.java
@@ -7,38 +7,21 @@ import java.io.UnsupportedEncodingException;
 public class EncodingTool {
 
     /**
-     * 判断编码基础方法
+     * 将传入的字符串，转为utf-8格式
      * @param value
      * @param charset
      * @return
      */
-    public static boolean isEncodingByCharset(String value, String charset) {
+    public static String encodingByCharset(String value, String charset) {
+        if(ValidateTool.isEmpty(value)) {
+            return null;
+        }
         try {
-            if (value.equals(new String(value.getBytes(), charset))) {
-                return true;
-            }
+            return new String(value.getBytes(), charset);
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
-        return false;
-    }
-
-    /**
-     * 判断字符串编码格式是否为 ISO8859
-     * @param value
-     * @return
-     */
-    public static boolean isISO8859(String value) {
-        return isEncodingByCharset(value, CharsetConstant.CHARSET_ISO8859);
-    }
-
-    /**
-     * 判断字符串编码格式是否为 UTF-8
-     * @param value
-     * @return
-     */
-    public static boolean isUTF8(String value) {
-        return isEncodingByCharset(value, CharsetConstant.CHARSET_UTF8);
+        return null;
     }
 
     /**
@@ -48,7 +31,10 @@ public class EncodingTool {
      * @param toCharset
      * @return
      */
-    public static String toEncoding(String value, String fromCharset, String toCharset) {
+    public static String encoding(String value, String fromCharset, String toCharset) {
+        if(ValidateTool.isEmpty(value)) {
+            return null;
+        }
         try {
             return new String(value.getBytes(fromCharset), toCharset);
         } catch (UnsupportedEncodingException e) {
@@ -58,25 +44,11 @@ public class EncodingTool {
     }
 
     /**
-     * ISO8859编码 转为 UTF8编码
-     * @param value
-     * @return
-     */
-    public static String iso8859ToUTF8(String value) {
-        return toEncoding(value, CharsetConstant.CHARSET_ISO8859, CharsetConstant.CHARSET_UTF8);
-    }
-
-    /**
      * 转为 UTF-8 编码
      * @param value
      * @return
      */
-    public static String toUTF8(String value) {
-        if(isUTF8(value)) {
-            return value;
-        } else if (isISO8859(value)) {
-            return iso8859ToUTF8(value);
-        }
-        return null;
+    public static String encodingUTF8(String value) {
+        return encodingByCharset(value, CharsetConstant.CHARSET_UTF8);
     }
 }
